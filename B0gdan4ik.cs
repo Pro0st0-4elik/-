8c320967f3528d1d78a283a754b1946d46f2b755
Pro0st0-4elik <152833166+Pro0st0-4elik@users.noreply.github.com>
2023-12-04 08:44:07 +0500

    Цикл While с X и Y

---
+++ B0gdan4ik.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+while(x <= y)
+{
+    Console.WriteLine(x);
+    x++;
+}
